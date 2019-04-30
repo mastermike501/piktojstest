06a9410d0443c8c6375acdd630985f965bbb4937
Michael Kong <michael.kong@intel.com>
2019-04-30 16:25:54 +0800

    First working version

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidepane />
+    <Canvas />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidepane from './components/Sidepane.vue'
+import Canvas from './components/Canvas.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sidepane,
+    Canvas
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
